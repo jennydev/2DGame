24038e73871f9beba1326ea8f6b6c3e3a52c008d
Jenia Zavalova <Jenia.Zavalova@novarto.com>
2016-12-08 18:13:28 +0200

    assets added

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,59 @@
 var rows = 8;
 var mapData = [];
 var gemsMatch = [];
+var selectAllowed = true;
+var first = true;
+var soundOn = true;
+var stats = {
+    score:0,
+    smashing:0,
+    splendid:0,
+    indubitably:0,
+    marvelous:0,
+    unbelievable:0,
+    beer1:0,
+    beer2:0
+}
+var combo = 0;
+
+var sounds = {
+    start: "assets/sounds/start.mp3",
+    smashing: "assets/sounds/smashing.mp3",
+    splendid: "assets/sounds/splendid.mp3",
+    marvelous: "assets/sounds/marvelous.mp3",
+    unbelievable: "assets/sounds/unbelievable.mp3",
+    select: "assets/sounds/select.mp3",
+    move: "assets/sounds/move.mp3",
+    disabledmove: "assets/sounds/disabledmove.mp3",
+    first:"assets/sounds/first_of_many.mp3",
+    indubitably:"assets/sounds/indubitably.mp3",
+    destroy:"assets/sounds/destroy.mp3"
+
+};
 
 
 function start() {
-    generateMap();
-    bindEvents();
-}
+    //console.log('start()');
 
-function bindEvents() {
-    console.log('bind events')
+    generateMap();
+
     $("#new-game").on("click", function () {
         generateMap();
-        bindEvents();
 
-        playAnimation($(".destroy-gems"))
+    });
+
+    $('.sound-icon').on('click', function(){
+        //console.log($(this));
+        $(this).toggleClass('disabled-sound');
+        soundOn = !soundOn;
     })
+}
 
-    $(".block").on("click", function () {
+function bindBlockClick($element) {
+
+
+    $element.on("click", function () {
+
 
         // Gem2 is the second selected gem
         var $gem2 = $(this);
@@ -27,6 +63,11 @@ function bindEvents() {
         $gem1.removeClass("active");
         $gem2.addClass("active");
 
+        playSound(sounds.select);
+
+        if (!selectAllowed) {
+            return;
+        }
 
         //If there is a previously selected gem get coordinates of Gem1 and Gem2
         if ($gem1.length > 0) {
@@ -41,6 +82,9 @@ function bindEvents() {
             // If the distance between Gem1 and Gem2 is lower or equal to 1 switch Gem1 and Gem2 positions
             if (Math.abs(gem1Row - gem2Row) <= 1 && Math.abs(gem1Col - gem2Col) <= 1) {
 
+                playSound(sounds.move);
+
+                selectAllowed = false;
                 var gem1InMap = mapData[gem1Row][gem1Col].gemType;
                 var gem2InMap = mapData[gem2Row][gem2Col].gemType;
                 var gem1Image = mapData[gem1Row][gem1Col].image;
@@ -52,20 +96,16 @@ function bindEvents() {
                 mapData[gem1Row][gem1Col].gemType = gem2InMap;
                 mapData[gem2Row][gem2Col].gemType = gem1InMap;
 
-                console.log(mapData[gem1Row][gem1Col].gemType, mapData[gem2Row][gem2Col].gemType)
-
-                console.log("row offset: ", Math.abs(gem1Row - gem2Row), "col offset:", Math.abs(gem1Col - gem2Col));
-
                 //Get Gem2 position
-                var swapTop1 = gem2Row * 80;
-                var swapLeft1 = gem2Col * 80;
+                var swapTop1 = gem2Row * 81;
+                var swapLeft1 = gem2Col * 81;
 
                 // Animate Gem1 to Gem2 position
-                $gem1.animate({left: swapLeft1 + "px", top: swapTop1 + 'px'});
+                $gem1.animate({left: swapLeft1 + "px", top: swapTop1 + 'px'}, 300);
 
                 //Get Gem1 position
-                var swapTop2 = gem1Row * 80;
-                var swapLeft2 = gem1Col * 80;
+                var swapTop2 = gem1Row * 81;
+                var swapLeft2 = gem1Col * 81;
 
                 // Switch attributes col and row between Gem1 and Gem2
                 $gem2.attr({'col': gem1Col, 'row': gem1Row});
@@ -80,12 +120,36 @@ function bindEvents() {
                 $gem2.removeClass("active");
 
 
+
                 // Animate Gem2 to Gem1 position
-                $gem2.animate({left: swapLeft2 + "px", top: swapTop2 + 'px'}, 500, function () {
+                $gem2.animate({left: swapLeft2 + "px", top: swapTop2 + 'px'}, 300, function () {
                     // Animation complete.
 
+
                     checkGems(mapData);
-                    destroyGems(gemsMatch)
+                    if (gemsMatch.length > 0) {
+                        destroyGems(gemsMatch)
+                    } else {
+                        // Move gem back
+                        playSound(sounds.disabledmove);
+                        $gem2.animate({left: swapLeft1 + "px", top: swapTop1 + 'px'}, 300, function () {
+                            selectAllowed = true;
+                        });
+                        $gem1.animate({left: swapLeft2 + "px", top: swapTop2 + 'px'}, 300, function () {
+
+                        });
+
+                        $gem2.attr({'col': gem2Col, 'row': gem2Row});
+                        $gem1.attr({'col': gem1Col, 'row': gem1Row});
+
+                        mapData[gem2Row][gem2Col].image = gem2Image;
+                        mapData[gem1Row][gem1Col].image = gem1Image;
+
+                        mapData[gem2Row][gem2Col].gemType = gem2InMap;
+                        mapData[gem1Row][gem1Col].gemType = gem1InMap;
+
+
+                    }
 
                 });
 
@@ -96,10 +160,30 @@ function bindEvents() {
         }
 
     })
-
 }
 
+
 function generateMap() {
+
+    playSound(sounds.start);
+    addImageAnimation('start');
+
+    stats = {
+        score:0,
+        smashing:0,
+        splendid:0,
+        indubitably:0,
+        marvelous:0,
+        unbelievable:0,
+        beer1:0,
+        beer2:0
+    }
+    first= true;
+
+
+
+    updateScore()
+    //console.log('generateMap()');
 
 
     //rows
@@ -145,14 +229,12 @@ function generateMap() {
 
     }
 
-    console.log('map data', mapData)
+    //console.log('map data', mapData)
     renderGems(mapData)
 
 }
 
 function checkGems(mapData) {
-
-
     gemsMatch = [];
     for (var row = 0; row < mapData.length; row++) {
         for (var col = 0; col < mapData[row].length; col++) {
@@ -164,40 +246,109 @@ function checkGems(mapData) {
 
     gemsMatch = _.uniq(gemsMatch);
 
-    var verticalMatched = _.where(gemsMatch, {vertical: true});
+    var beer1Length = _.where(gemsMatch, {'gemType':5}).length;
+    var beer2Length = _.where(gemsMatch, {'gemType':6}).length;
+    stats.beer1 += beer1Length;
+    stats.beer2 += beer2Length;
 
-    if (verticalMatched.length > 0) {
+    if (gemsMatch.length > 0) {
+        playSound(sounds.destroy);
+    }
+    if(gemsMatch.length == 4){
 
-        var firstArray = _.where(verticalMatched, {gemType: verticalMatched[0].gemType});
-        var secondArray = _.reject(verticalMatched, {gemType: verticalMatched[0].gemType});
-        console.log('--->', firstArray, secondArray);
+        playSound(sounds.splendid);
+        addImageAnimation("splendid");
+        stats.splendid++;
 
-        var firstOffset = firstArray[0].row - 0;
 
-        for (var i = 0; i < firstOffset; i++) {
+    }
+    if(gemsMatch.length >= 5){
+        addImageAnimation('indubitably');
+        playSound(sounds.indubitably);
+        stats.indubitably++;
+    }
 
-            var $gem = $(".block[row='" + i + "'][col='" + firstArray[0].col + "']");
-            var gemOffsetTop = (i + firstArray.length) * 80;
+    updateScore()
+}
 
-            var gemInMap = mapData[i + firstArray.length][firstArray[0].col];
-            gemInMap.gemType = mapData[i][firstArray[0].col].gemType;
 
-            console.log(gemInMap.gemType);
 
-            $gem.animate({top: gemOffsetTop + 'px'}, 500, function () {
+function moveGemsDown(gemsMatch) {
 
-                //$gem
 
-            });
+    for (var col = 0; col < rows; col++) {
 
+        var emptyGemsOnCol = _.where(gemsMatch, {col: col});
+
+        if (emptyGemsOnCol.length > 0) {
+            //console.log('empty gems on col:', col, ':', emptyGemsOnCol)
         }
+
+
+        for (var row = mapData.length - 1; row >= 0; row--) {
+            _.each(emptyGemsOnCol, function (emptyGem, index) {
+
+                var currentGem = mapData[row][col];
+
+                //move existing gems
+                if (currentGem.row < emptyGem.row) {
+
+
+                    var emptyGemsBelow = _.filter(emptyGemsOnCol, function (i) {
+                        return i.row > currentGem.row;
+                    })
+
+
+                    var rowOffset = currentGem.row + emptyGemsBelow.length;
+                    var gemOffsetTop = rowOffset * 81;
+
+                    var $gem = $('#gem-container').find(".block[row='" + currentGem.row + "'][col='" + currentGem.col + "']");
+
+                    var matchedGem = mapData[rowOffset][currentGem.col];
+                    matchedGem.gemType = currentGem.gemType;
+                    matchedGem.image = "assets/img/gem_" + currentGem.gemType + ".png";
+
+                    $gem.attr('row', rowOffset);
+                    $gem.find('.helper-info').html(rowOffset + ":" + col);
+
+                    $gem.animate({top: gemOffsetTop + 'px'}, 500, function () {
+
+                    });
+                }
+            })
+        }
+
+        //create new gems and move them
+        _.each(emptyGemsOnCol, function (emptyGem, index) {
+
+            var randomType = getRandomizer(1, 7);
+            var currentRow = emptyGemsOnCol.length - index - 1;
+            var $newDiv = $('<div class="block white-block text-center" row="' + currentRow + '" col="' + col + '"><span class="helper"><span class="helper-info">' + currentRow + ":" + col + '</span> </span></div>');
+            var $img = $('<img class="gem-img"/>');
+            $img.attr('src', "assets/img/gem_" + randomType + ".png");
+
+            $newDiv.find('.helper').append($img);
+            $newDiv.css({'top': (-1 - index) * 81, 'left': col * 81});
+
+            $('#gem-container').append($newDiv);
+            mapData[currentRow][col].gemType = randomType;
+            mapData[currentRow][col].image = "assets/img/gem_" + mapData[currentRow][col].gemType + ".png",
+
+                $newDiv.animate({top: (emptyGemsOnCol.length - 1 - index) * 81 + 'px'}, 500, function () {
+                    bindBlockClick($newDiv);
+                });
+
+        });
+
 
     }
 
 
 }
 
+
 function renderGems(mapData) {
+    //console.log('renderGems()');
 
 
     $('#gem-container').empty();
@@ -216,15 +367,17 @@ function renderGems(mapData) {
             var $img = $('<img class="gem-img"/>');
             $img.attr('src', mapData[row][col].image);
             $newDiv.find('.helper').append($img)
-            $newDiv.css({'top': row * 80, 'left': col * 80});
+            $newDiv.css({'top': row * 81, 'left': col * 81});
 
             $('#gem-container').append($newDiv);
         }
     }
 
-    console.log('render gems')
-}
+    $('.block').each(function () {
+        bindBlockClick($(this));
+    });
 
+}
 
 function getRandomizer(bottom, top) {
     return Math.floor(Math.random() * ( 1 + top - bottom )) + bottom;
@@ -233,18 +386,20 @@ function getRandomizer(bottom, top) {
 function getRandomGem(row, col, gemType, initialLoad) {
 
     var gemTypeRepeats = false;
+    var gemOnTop = 0;
+    var gemOnLeft = 0;
 
     if (mapData[row] && mapData[row][col - 1] && mapData[row][col - 2]) {
 
         //Checks the previous two on the same row
         if (gemType == mapData[row][col - 1].gemType && gemType == mapData[row][col - 2].gemType) {
-            console.log("gems on row at: [", row, ":", col - 2, "]", "[", row, ":", col - 1, "]", "[", row, ":", col, "] are same type: ", gemType)
+            //console.log("gems on row at: [", row, ":", col - 2, "]", "[", row, ":", col - 1, "]", "[", row, ":", col, "] are same type: ", gemType)
             gemTypeRepeats = true;
             // checks if initialLoad is false
             if (!initialLoad) {
-                gemsMatch.push(mapData[row][col]);
-                gemsMatch.push(mapData[row][col - 1]);
                 gemsMatch.push(mapData[row][col - 2]);
+                gemsMatch.push(mapData[row][col - 1]);
+                gemsMatch.push(mapData[row][col]);
             }
         }
 
@@ -254,10 +409,9 @@ function getRandomGem(row, col, gemType, initialLoad) {
     if (mapData[row] && mapData[row - 1] && mapData[row - 2]) {
 
         if (gemType == mapData[row - 1][col].gemType && gemType == mapData[row - 2][col].gemType) {
-            console.log("gems at: [", row - 2, ":", col, "]", "[", row - 1, ":", col, "]", "[", row, ":", col, "] are same type: ", gemType)
+            //console.log("gems at: [", row - 2, ":", col, "]", "[", row - 1, ":", col, "]", "[", row, ":", col, "] are same type: ", gemType)
             gemTypeRepeats = true;
             if (!initialLoad) {
-                mapData[row][col].vertical = mapData[row - 2][col].vertical = mapData[row - 1][col].vertical = true;
                 gemsMatch.push(mapData[row - 2][col]);
                 gemsMatch.push(mapData[row - 1][col]);
                 gemsMatch.push(mapData[row][col]);
@@ -265,68 +419,111 @@ function getRandomGem(row, col, gemType, initialLoad) {
         }
 
     }
-    //Checks the top left diagonal the two gems
-    if (mapData[row] && mapData[row - 1] && mapData[row - 2] && mapData[row - 1][col - 1] && mapData[row - 2][col - 2]) {
-
-        if (gemType == mapData[row - 1][col - 1].gemType && gemType == mapData[row - 2][col - 2].gemType) {
-            console.log("gems at: [", row - 2, ":", col - 2, "]", "[", row - 1, ":", col - 1, "]", "[", row, ":", col, "] are same type: ", gemType)
-            gemTypeRepeats = true;
-            if (!initialLoad) {
-                gemsMatch.push(mapData[row][col]);
-                gemsMatch.push(mapData[row - 1][col - 1]);
-                gemsMatch.push(mapData[row - 2][col - 2]);
-            }
-        }
-    }
-    //Checks the top right diagonal the two gems
-    if (mapData[row] && mapData[row - 1] && mapData[row - 1][col + 1] && mapData[row - 2] && mapData[row - 2][col + 2]) {
-
-        if (gemType == mapData[row - 1][col + 1].gemType && gemType == mapData[row - 2][col + 2].gemType) {
-            console.log("gems at: [", row - 2, ":", col + 2, "]", "[", row - 1, ":", col + 1, "]", "[", row, ":", col, "] are same type: ", gemType)
-            gemTypeRepeats = true;
-            if (!initialLoad) {
-                gemsMatch.push(mapData[row][col]);
-                gemsMatch.push(mapData[row - 1][col + 1]);
-                gemsMatch.push(mapData[row - 2][col + 2]);
-            }
-        }
-    }
+    ////Checks the top left diagonal the two gems
+    //if (mapData[row] && mapData[row - 1] && mapData[row - 2] && mapData[row - 1][col - 1] && mapData[row - 2][col - 2]) {
+    //
+    //    if (gemType == mapData[row - 1][col - 1].gemType && gemType == mapData[row - 2][col - 2].gemType) {
+    //        console.log("gems at: [", row - 2, ":", col - 2, "]", "[", row - 1, ":", col - 1, "]", "[", row, ":", col, "] are same type: ", gemType)
+    //        gemTypeRepeats = true;
+    //        if (!initialLoad) {
+    //            gemsMatch.push(mapData[row - 2][col - 2]);
+    //            gemsMatch.push(mapData[row - 1][col - 1]);
+    //            gemsMatch.push(mapData[row][col]);
+    //        }
+    //    }
+    //}
+    ////Checks the top right diagonal the two gems
+    //if (mapData[row] && mapData[row - 1] && mapData[row - 1][col + 1] && mapData[row - 2] && mapData[row - 2][col + 2]) {
+    //
+    //    if (gemType == mapData[row - 1][col + 1].gemType && gemType == mapData[row - 2][col + 2].gemType) {
+    //        console.log("gems at: [", row - 2, ":", col + 2, "]", "[", row - 1, ":", col + 1, "]", "[", row, ":", col, "] are same type: ", gemType)
+    //        gemTypeRepeats = true;
+    //        if (!initialLoad) {
+    //            gemsMatch.push(mapData[row][col]);
+    //            gemsMatch.push(mapData[row - 1][col + 1]);
+    //            gemsMatch.push(mapData[row - 2][col + 2]);
+    //        }
+    //    }
+    //}
 
 //If  there are at least 3 matches of one gemType it replaces the gemType of the current one
 
     if (gemTypeRepeats && initialLoad) {
-        var factor = gemType / 4;
-        if (factor > 1) {
-            gemType = gemType - getRandomizer(1, gemType - 1);
+
+        if (gemType > 4) {
+            gemType = 8 - gemType
         } else {
-            gemType = gemType + getRandomizer(1, gemType - 1);
+            gemType = 7 - gemType;
         }
+
     }
+
 
     return gemType;
 }
 
 function destroyGems(gemsArray) {
 
-
-    for (i = 0; i < gemsArray.length; i++) {
+    selectAllowed = false;
+    for (var i = 0; i < gemsArray.length; i++) {
 
         var $destroyAnimation = $('<div/>', {
             class: 'destroy-gems'
         });
 
-        var $gem = $(".block[row='" + gemsArray[i].row + "'][col='" + gemsArray[i].col + "']");
+        var $gem = $('#gem-container').find(".block[row='" + gemsArray[i].row + "'][col='" + gemsArray[i].col + "']");
         $gem.find('img').hide();
         $gem.append($destroyAnimation);
         playAnimation($destroyAnimation)
 
     }
 
+    setTimeout(function () {
+
+        moveGemsDown(gemsMatch);
+        setTimeout(function () {
+            checkGems(mapData);
+
+            if (gemsMatch.length > 0) {
+                combo++;
+
+                destroyGems(gemsMatch)
+            } else {
+                if(combo == 1){
+
+                    playSound(sounds.smashing);
+                    addImageAnimation("smashing");
+                    stats.smashing++;
+                    stats.score += 100;
+
+                }
+                if(combo == 2){
+                    addImageAnimation('marvelous');
+                    playSound(sounds.marvelous);
+                    stats.marvelous++;
+                    stats.score += 200;
+
+                }
+                if(combo >= 3){
+                    addImageAnimation('unbelievable');
+                    playSound(sounds.unbelievable);
+                    stats.unbelievable++;
+                    stats.score += 400;
+
+
+                }
+                updateScore()
+                combo = 0;
+                selectAllowed = true;
+
+            }
+        }, 500)
+    }, 500);
+
 
 }
 
 function playAnimation($destroyAnimation) {
-
     $destroyAnimation.animateSprite({
         fps: 25,
         loop: false,
@@ -342,13 +539,60 @@ function playAnimation($destroyAnimation) {
 
     $destroyAnimation.animateSprite("play", "start");
 
+
+}
+
+function updateScore() {
+
+
+    if (gemsMatch.length == 3) {
+        stats.score += 30;
+    }
+    if (gemsMatch.length == 4) {
+        stats.score += 60;
+    }
+    if (gemsMatch.length > 4) {
+        stats.score += 60 + gemsMatch.length * 10
+    }
+
+    //console.log('All matched gems: ', gemsMatch.length)
+
+    for (var property in stats) {
+        if (stats.hasOwnProperty(property)) {
+            var value = stats[property];
+            $('.' + property).html(value);
+        }
+    }
+
+
 }
 
 
+function playSound(sound){
+
+    if(!soundOn){
+        return;
+    }
+    var sound = new Audio(sound)
+    sound.play()
+}
 
 
+function addImageAnimation(imageClass, duration){
+    $('.inner-container').find('img').hide();
+    if(duration == undefined){
+        duration = 1200;
+    }
+    var $image = $('.inner-container').find('.'+ imageClass)
+    $image.show()
+    $image.addClass('shake');
 
+    setTimeout(function () {
+        $image.removeClass('shake');
+        $image.fadeOut('fast')
+    }, duration)
 
+}
 
 
 
